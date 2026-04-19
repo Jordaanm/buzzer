08ae48651c1c05540267380169e7be6ce78445ae
Jordaan <jordaanm@gmail.com>
2026-04-19 23:00:30 +1000

    ci: temp log server_url in GHA

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,5 +1,6 @@
 import { io } from 'socket.io-client';
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL ?? 'http://localhost:3001';
+console.log('SERVER_URL', SERVER_URL, import.meta.env.VITE_SERVER_URL);
 
 export const socket = io(SERVER_URL, { autoConnect: false });
